61459c2852b0a9f3597f96bcec53742c38b53575
Omar Ramos <omarramos755@gmail.com>
2023-10-07 03:05:15 -0600

    renumbre de carpeta datasources

--- lib/features/inventory/infrastructure/infrastructure.dart
+++ lib/features/inventory/infrastructure/infrastructure.dart
@@ -1,4 +1,4 @@
-export 'datasource/products_datasource_impl.dart';
+export 'datasources/products_datasource_impl.dart';
 export 'mappers/product_mapper.dart';
 export 'repositories/products_repository_impl.dart';
 // export 'errors/product_errors.dart';
