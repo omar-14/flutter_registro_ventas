f8a2c3a6e7547cd23a602b9bd84ca6c77a99c513
Omar Ramos <omarramos755@gmail.com>
2023-10-22 15:41:48 -0600

    provider en sidebar

--- lib/features/shared/widgets/side_menu.dart
+++ lib/features/shared/widgets/side_menu.dart
@@ -1,24 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 import 'package:intventory/config/router/menu_items.dart';
+import 'package:intventory/features/auth/presentation/providers/providers.dart';
 import 'custom_filled_button.dart';
 
-class SideMenu extends StatefulWidget {
+class SideMenu extends ConsumerStatefulWidget {
   final GlobalKey<ScaffoldState> scaffoldKey;
 
   const SideMenu({super.key, required this.scaffoldKey});
 
   @override
-  State<SideMenu> createState() => _SideMenuState();
+  SideMenuState createState() => SideMenuState();
 }
 
-class _SideMenuState extends State<SideMenu> {
-  int navDrawerIndex = 3;
+class SideMenuState extends ConsumerState<SideMenu> {
+  int navDrawerIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     final hasNotch = MediaQuery.of(context).viewPadding.top > 35;
     final textStyles = Theme.of(context).textTheme;
+
+    final userAuth = ref.read(authProvider);
 
     return NavigationDrawer(
         elevation: 1,
@@ -30,7 +34,7 @@ class _SideMenuState extends State<SideMenu> {
 
           final menuItems = appMenuItems[value];
 
-          context.pushReplacement(menuItems.link);
+          context.push(menuItems.link);
 
           widget.scaffoldKey.currentState?.closeDrawer();
         },
@@ -41,7 +45,9 @@ class _SideMenuState extends State<SideMenu> {
           ),
           Padding(
             padding: const EdgeInsets.fromLTRB(20, 0, 16, 10),
-            child: Text('Tony Stark', style: textStyles.titleSmall),
+            child: Text(
+                "${userAuth.user!.firstName} ${userAuth.user!.lastName}",
+                style: textStyles.titleSmall),
           ),
           ...appMenuItems.map((menuItem) => NavigationDrawerDestination(
               icon: Icon(menuItem.icon), label: Text(menuItem.title))),
@@ -53,7 +59,8 @@ class _SideMenuState extends State<SideMenu> {
             padding: const EdgeInsets.symmetric(horizontal: 20),
             child: CustomFilledButton(
                 onPressed: () {
-                  // ref.read(authProvider.notifier).logout();
+                  ref.read(authProvider.notifier).logout();
+                  context.go("/login");
                 },
                 text: 'Cerrar sesión'),
           )
