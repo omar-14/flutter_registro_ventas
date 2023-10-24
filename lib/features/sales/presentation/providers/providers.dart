3ff4c03cf6f4b0bd52f8f45b6a29b27b0ebf796f
Omar Ramos <omarramos755@gmail.com>
2023-10-24 02:21:24 -0600

    archivos faltantes

--- lib/features/sales/presentation/providers/providers.dart
+++ lib/features/sales/presentation/providers/providers.dart
@@ -2,7 +2,6 @@ export 'sale/sales_provider.dart';
 export 'sale/sale_provider.dart';
 export 'sale/sales_repository_provider.dart';
 
-export 'detail_sale/detail_sale_provider.dart';
 export 'detail_sale/details_sales_provider.dart';
 export 'detail_sale/details_sales_repository_provider.dart';
 
--- lib/features/shared/widgets/scanner_qr.dart
+++ lib/features/shared/widgets/scanner_qr.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 class ScannerQr extends StatelessWidget {
   const ScannerQr({super.key});
@@ -11,14 +11,14 @@ class ScannerQr extends StatelessWidget {
       elevation: 0,
       child: const Icon(Icons.filter_center_focus),
       onPressed: () async {
-        String barcodeScanRes;
-        try {
-          barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
-              '#ff6666', 'Cancel', true, ScanMode.QR);
-          print(barcodeScanRes);
-        } on PlatformException {
-          barcodeScanRes = 'Failed to get platform version.';
-        }
+        // String barcodeScanRes;
+        // try {
+        //   barcodeScanRes = await FlutterBarcodeScanner.scanBarcode(
+        //       '#ff6666', 'Cancel', true, ScanMode.QR);
+        //   // print(barcodeScanRes);
+        // } on PlatformException {
+        //   barcodeScanRes = 'Failed to get platform version.';
+        // }
       },
     );
   }
